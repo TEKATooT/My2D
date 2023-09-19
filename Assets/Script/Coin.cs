e2fb0b90ea8d0592dff9015a5b95571c4cd38dda
TekinTooT <63135446+TekinTooT@users.noreply.github.com>
2023-09-19 21:55:06 +0300

    5.3.1

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+            Destroy(gameObject);
+    }
 }
--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyMove : MonoBehaviour
 {
+    [SerializeField] private int patrolLength;
+    [SerializeField] private int patrolTime;
+
     private void Start()
     {
-        
-    }
+        float _startPosition = transform.position.x;
 
-    private void Update()
-    {
-        
+        _startPosition += patrolLength;
+
+        transform.DOLocalMoveX(_startPosition, patrolTime, false).SetLoops(-1, LoopType.Yoyo);
     }
 }
