ced3df9ef06dde7819f844a6d35bb86adea89d20
TekinTooT <dendendendenisss@gmail.com>
2023-09-28 22:02:35 +0300

    6.2(4)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _upSlider;
+    [SerializeField] private UnityEvent _downSlider;
+
     private float _health = 1;
     private float _startHealth = 1;
     private float _heal = .1f;
     private float _damage = -.1f;
     private float _zeroHealth = 0;
 
+    public float Health => _health;
     public void GiveHeal()
     {
         if (_health < _startHealth)
         {
             _health += _heal;
+
+            _upSlider.Invoke();
         }
     }
 
@@ -25,6 +32,8 @@ public class Player : MonoBehaviour
         if (_health > _zeroHealth)
         {
             _health += _damage;
+
+            _downSlider.Invoke();
         }
     }
 }
