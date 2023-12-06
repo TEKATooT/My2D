f306aaedd4081ee388548e9d20564816b73888c7
TekinTooT <63135446+TekinTooT@users.noreply.github.com>
2023-12-06 21:44:59 +0300

    5.7

--- Assets/Other/Script/Coin.cs
+++ Assets/Other/Script/Coin.cs
@@ -4,7 +4,7 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<Player2d>(out Player2d player))
+        if (collision.TryGetComponent<Player>(out Player player))
             Destroy(gameObject);
     }
 }
