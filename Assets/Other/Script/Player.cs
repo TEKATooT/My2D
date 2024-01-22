c4b9c2286d8ca7f1d5723c078ff9938fa7ff872c
TekinTooT <dendendendenisss@gmail.com>
2024-01-22 18:58:10 +0300

    LiveSteel

--- Assets/Other/Script/Player.cs
+++ Assets/Other/Script/Player.cs
@@ -1,5 +1,52 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : AbstractWarrior
 {
+    [SerializeField] private Transform _frontPosition;
+    [SerializeField] private int _timeForSteal;
+    [SerializeField] private float _skillRange;
+    [SerializeField] private float _steelValue;
+    [SerializeField] private float _liveStealcooldown = 10;
+
+    [SerializeField] private Player _self;
+    [SerializeField] private AbstractWarrior _target;
+
+    private float _cooldown = 0;
+    private bool _isReady = true;
+
+    private void Update()
+    {
+        SkillLiveSteal();
+    }
+
+    private void SkillLiveSteal()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && _isReady == true)
+        {
+            RaycastHit2D target = Physics2D.Raycast(_frontPosition.position, transform.right, _skillRange);
+
+            if (target)
+            {
+                if (_target.gameObject == target.collider.gameObject)
+                {
+                    StartCoroutine(LiveSteal(_self, _target, _steelValue, _timeForSteal));
+
+                    _cooldown = _liveStealcooldown;
+                    _isReady = false;
+
+                    StartCoroutine(CooldownTimer());
+                }
+            }
+        }
+    }
+
+    private IEnumerator CooldownTimer()
+    {
+        var waitForSecond = new WaitForSeconds(_liveStealcooldown);
+
+        yield return waitForSecond;
+
+        _isReady = true;
+    }
 }
